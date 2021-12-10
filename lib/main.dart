649ea514122179665a42158e3f8a565606e0e6e4
derek.zeng <derek.zeng@bytedance.com>
2021-12-10 13:47:26 +0800

    feat: credential crud

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'models/credential.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider(
+      create: (BuildContext context) {
+        return Vault(secret: 'abcdabcdabcdabcd');
+      },
+      child: const MyApp()));
 }
-
-// key must be length 16
-var vault = Vault('abcdabcdabcdabcd');
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -19,7 +21,7 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.green,
         ),
         home: const MyHomePage(title: 'Vault'),
-        routes: {'/new': (context) => const CredentialForm()});
+        routes: {'/form': (context) => const CredentialForm()});
   }
 }
 
@@ -35,13 +37,46 @@ class MyHomePage extends StatelessWidget {
         title: Text(title),
       ),
       body: Center(
-        child: ListView(
-            children: vault.credentials
-                .map((cred) => ListTile(title: Text(cred.username)))
-                .toList()),
-      ),
+          child: Consumer<Vault>(
+        builder: (context, vault, child) => vault.credentials.isNotEmpty
+            ? ListView.separated(
+                itemCount: vault.credentials.length,
+                separatorBuilder: (BuildContext context, int index) =>
+                    const Divider(thickness: 1),
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    title: Text(vault.credentials[index].username),
+                    onTap: () {
+                      Navigator.pushNamed(context, '/form',
+                          arguments: vault.credentials[index]);
+                    },
+                    subtitle: Text(vault.credentials[index].description ?? ''),
+                    trailing: IconButton(
+                        icon: const Icon(Icons.delete),
+                        onPressed: () {
+                          showDialog(
+                              context: context,
+                              builder: (context) => AlertDialog(
+                                      title:
+                                          const Text('Are you sure to delete?'),
+                                      content:
+                                          const Text('Yes, just delete it'),
+                                      actions: [
+                                        TextButton(
+                                            child: const Text('Ok'),
+                                            onPressed: () {
+                                              vault.remove(
+                                                  vault.credentials[index]);
+                                              Navigator.pop(context, 'OK');
+                                            }),
+                                      ]));
+                        }),
+                  );
+                })
+            : const Center(child: Text('No credentials')),
+      )),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => Navigator.pushNamed(context, '/new'),
+        onPressed: () => Navigator.pushNamed(context, '/form'),
         tooltip: 'New credential',
         child: const Icon(Icons.add),
       ),
@@ -60,57 +95,191 @@ class _CredentialForm extends State<CredentialForm> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   String? _username;
   String? _password;
+  String? _url;
+  String? _description;
+  String? _category;
+  String? _tmpDialogCat;
+  bool _showPassword = false;
+
+  final List<String> _newCat = [];
 
   @override
   Widget build(BuildContext context) {
+    final cred = ModalRoute.of(context)!.settings.arguments as Credential?;
+    // if (cred?.category != null) {
+    //   _newCat.add(cred!.category!);
+    // }
+    List<String> categories = _newCat
+      ..addAll(Provider.of<Vault>(context).categories);
+
     return Scaffold(
         appBar: AppBar(
-          title: const Text('Create a new credential'),
+          title: cred == null
+              ? const Text('Create a new credential')
+              : const Text('Edit credential'),
         ),
-        body: Form(
+        body: SingleChildScrollView(
+            keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+            child: Form(
             key: _formKey,
             child: Padding(
                 padding: const EdgeInsets.all(20),
                 child: Column(
+                    mainAxisSize: MainAxisSize.max,
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      TextFormField(
-                          onSaved: (s) => _username = s,
-                          validator: (s) {
-                            if (s == '') {
-                              return 'Required field';
-                            }
-                            return null;
-                          },
+                      Padding(
+                          padding: const EdgeInsets.only(top: 10),
+                          child: TextFormField(
+                              initialValue: cred?.username,
+                              onSaved: (s) => _username = s,
+                              validator: (s) {
+                                if (s == '') {
+                                  return 'Required field';
+                                }
+                                return null;
+                              },
+                              decoration: const InputDecoration(
+                                  hintText: 'Enter username'))),
+                      Padding(
+                          padding: const EdgeInsets.only(top: 10),
+                          child: TextFormField(
+                              initialValue: cred?.password,
+                              onSaved: (s) => _password = s,
+                              obscureText: !_showPassword,
+                              decoration: InputDecoration(
+                                  hintText: 'Enter password',
+                                  suffixIcon: IconButton(
+                                      icon: Icon(_showPassword? Icons.visibility: Icons.visibility_off),
+                                      onPressed: () {
+                                        setState(() {
+                                          _showPassword = !_showPassword;
+                                        });
+                                      })),
+                              validator: (s) {
+                                if (s == '') {
+                                  return 'Required field';
+                                }
+                                return null;
+                              })),
+                      Padding(
+                          padding: const EdgeInsets.only(top: 10),
+                          child: TextFormField(
+                              keyboardType: TextInputType.multiline,
+                              maxLines: null,
+                              initialValue: cred?.description,
+                              onSaved: (s) => _description = s,
+                              decoration: const InputDecoration(
+                                  hintText: 'Enter description'))),
+                      Padding(
+                          padding: const EdgeInsets.only(top: 10),
+                          child: TextFormField(
+                              initialValue: cred?.url,
+                              onSaved: (s) => _url = s,
+                              decoration: const InputDecoration(
+                                  hintText: 'Enter url'))),
+                      Row(children: [
+                        Expanded(
+                            child: DropdownButtonFormField<String>(
+                          onChanged: (s) {},
+                          value: cred?.category,
                           decoration: const InputDecoration(
-                              hintText: 'Enter username')),
-                      TextFormField(
-                          onSaved: (s) => _password = s,
-                          obscureText: true,
-                          decoration:
-                              const InputDecoration(hintText: 'Enter password'),
-                          validator: (s) {
-                            if (s == '') {
-                              return 'Required field';
+                              hintText: 'Select a category'),
+                          onSaved: (String? newValue) {
+                            if (newValue != null) {
+                              setState(() {
+                                _category = newValue;
+                              });
                             }
-                            return null;
-                          }),
+                          },
+                          items: categories
+                              .toSet()
+                              .toList()
+                              .map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                        )),
+                        Flexible(
+                            flex: 0,
+                            child: TextButton(
+                                child: const Text('Add'),
+                                onPressed: () {
+                                  showDialog<String>(
+                                    context: context,
+                                    builder: (BuildContext context) =>
+                                        AlertDialog(
+                                      title: const Text('Add category'),
+                                      content: Column(
+                                          mainAxisSize: MainAxisSize.min,
+                                          children: [
+                                            TextField(onChanged: (s) {
+                                              _tmpDialogCat = s;
+                                            }),
+                                          ]),
+                                      actions: <Widget>[
+                                        TextButton(
+                                          onPressed: () {
+                                            _tmpDialogCat = null;
+                                            Navigator.pop(context, 'Cancel');
+                                          },
+                                          child: const Text('Cancel'),
+                                        ),
+                                        TextButton(
+                                          onPressed: () {
+                                            if (_tmpDialogCat != null) {
+                                              setState(() {
+                                                _newCat.add(_tmpDialogCat!);
+                                              });
+                                            }
+                                            Navigator.pop(context, 'OK');
+                                          },
+                                          child: const Text('OK'),
+                                        ),
+                                      ],
+                                    ),
+                                  );
+                                })),
+                      ]),
                       Padding(
                           padding: const EdgeInsets.only(top: 10),
                           child: ElevatedButton(
                             onPressed: () {
                               if (_formKey.currentState!.validate()) {
                                 _formKey.currentState!.save();
-                                var cred =
-                                    Credential(_username!, _password!, '');
-                                setState(() {
-                                  vault.add(cred);
-                                });
-                                Navigator.pushReplacementNamed(context, '/');
+                                if (cred != null) {
+                                  cred
+                                    ..username = _username!
+                                    ..password = _password!
+                                    ..url = _url!
+                                    ..category = _category!
+                                    ..description = _description!;
+
+                                  setState(() {
+                                    Provider.of<Vault>(context, listen: false)
+                                        .update(cred);
+                                  });
+                                } else {
+                                  var cred = Credential(
+                                      username: _username!,
+                                      password: _password!,
+                                      url: _url,
+                                      category: _category,
+                                      description: _description);
+                                  setState(() {
+                                    Provider.of<Vault>(context, listen: false)
+                                        .add(cred);
+                                  });
+                                }
+                                Navigator.pop(context);
                               }
                             },
-                            child: const Text('Submit'),
+                            child: cred == null
+                                ? const Text('Create')
+                                : const Text('Save'),
                           )),
-                    ]))));
+                    ])))));
   }
 }
--- lib/models/credential.dart
+++ lib/models/credential.dart
@@ -1,48 +1,115 @@
 import 'dart:convert';
+import 'package:uuid/uuid.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:encrypt/encrypt.dart' as enc;
 
-import 'package:encrypt/encrypt.dart';
+var uuid = const Uuid();
 
 class Credential {
+  late String id;
   String username;
-  String password;
-  String hashedPassword;
+  String? description;
+  String? url;
+  String? category;
+  String? password;
+  String? hashedPassword;
 
-  Credential(this.username, this.password, this.hashedPassword);
+  Credential({required this.username, required this.password, this.description, this.category,
+      this.url, this.hashedPassword}) {
+    id = uuid.v4();
+  }
 
   @override
   String toString() {
     return '$username, $password, $hashedPassword';
   }
+
+  Credential.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        username = json['username'],
+        url = json['url'],
+        category = json['category'],
+        description = json['description'],
+        hashedPassword = json['hashedPassword'];
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'username': username,
+        'url': url,
+        'description': description,
+        'category': category,
+        'hashedPassword': hashedPassword,
+      };
 }
 
-class Vault {
-  List<Credential> credentials = [];
-  String key;
+class Vault extends ChangeNotifier {
+  final List<Credential> credentials = [];
+  final String secret;
 
-  Vault(this.key);
+  Vault({required this.secret}) {
+    init();
+  }
 
-  void lock() {}
-  void open() {}
+  Future<void> init() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    var jsonString = prefs.getStringList('store');
+    if (jsonString != null) {
+      for (String s in jsonString) {
+        var cred = Credential.fromJson(jsonDecode(s));
+        cred.password = decrypt(cred.hashedPassword!, secret);
+        credentials.add(cred);
+      }
+      notifyListeners();
+    }
+  }
+
+  List<String> get categories {
+    Set<String> ret = {};
+    for (var c in credentials) {
+      if (c.category != null) {
+        ret.add(c.category!);
+      }
+    }
+    return ret.toList();
+  }
+
+  Future<void> _persist() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setStringList(
+        'store', credentials.map((cred) => jsonEncode(cred.toJson())).toList());
+    notifyListeners();
+  }
+
   void add(Credential cred) {
-    cred.hashedPassword = encrypt(cred.password, key);
+    cred.hashedPassword = encrypt(cred.password!, secret);
     credentials.add(cred);
-    print('added new cred $cred');
+    _persist();
+  }
+
+  void remove(Credential cred) {
+    credentials.remove(cred);
+    _persist();
+  }
+
+  void update(Credential cred) {
+    _persist();
   }
 }
 
 String encrypt(String s, String k) {
-  final key = Key.fromUtf8(k);
-  final iv = IV.fromLength(16);
-  final encrypter = Encrypter(AES(key));
+  final key = enc.Key.fromUtf8(k);
+  final iv = enc.IV.fromLength(16);
+  final encrypter = enc.Encrypter(enc.AES(key));
 
   return encrypter.encrypt(s, iv: iv).base64;
 }
 
 String decrypt(String s, String k) {
-  final key = Key.fromUtf8(k);
-  final iv = IV.fromLength(16);
-  final encrypted = Encrypted(base64Decode(s));
-  final encrypter = Encrypter(AES(key));
+  final key = enc.Key.fromUtf8(k);
+  final iv = enc.IV.fromLength(16);
+  final encrypted = enc.Encrypted(base64Decode(s));
+  final encrypter = enc.Encrypter(enc.AES(key));
 
   return encrypter.decrypt(encrypted, iv: iv);
 }
